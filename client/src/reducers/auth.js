4c7a537c41de887012a2a5c52970e8f31083f053
harryramsden <harryramsden@yandex.com>
2020-08-03 10:59:52 +0100

    error

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,11 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+} from "../actions/types";
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -10,7 +17,15 @@ const initialState = {
 export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        payload,
+        isAuthenticated: true,
+        loading: false,
+      };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem("token", payload.token);
       return {
         ...state,
@@ -19,6 +34,8 @@ export default function (state = initialState, action) {
         loading: false,
       };
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem("token");
       return {
         ...state,
